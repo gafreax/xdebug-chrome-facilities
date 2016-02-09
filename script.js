4c7e6a2110eed4285364864210d64dd1ee4989d5
Gabriele Fontana <gafreax@gmail.com>
2016-02-09 12:05:28 +0100

    aggiunto file per js

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.write('loaded');
+    document.getElementById("btn_phpstorm_start").addEventListener("click",startPhpStorm);
+    document.getElementById("btn_phpstorm_stop").addEventListener("click",stopPhpStorm);
+});
+function startPhpStorm() {  
+    document.cookie='XDEBUG_SESSION='+'PHPSTORM'+';path=/;';
+    window.close();
+}
+function stopPhpStorm() {
+    document.cookie='XDEBUG_SESSION='+''+';expires=Mon, 05 Jul 2000 00:00:00 GMT;path=/;';
+    window.close();
+}
+
+
